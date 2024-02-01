633c40c35c0d46d779e2baa217e6f741e8774217
elizabethmck <157738237+elizabethmck@users.noreply.github.com>
2024-02-01 16:13:08 -0600

    test push

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 #REMOVE PASS AND FIX THE FUNCTION
 #change to test push and forks
 
-list1 = int(input())
-list2 = int(input())
-
-# def sum_of_products(list1, list2):
-    # this function should do the math
+def sum_of_products(list1, list2):
+    split1 = []
+    split2 = []
     
+    for i in list1:
+        if i.isspace():
+            continue
+        else:
+            split1.append(int(i))
+    for i in list2:
+        if i.isspace():
+            continue
+        else:
+            split2.append(int(i))
 
-if len(list1) == len(list2):
-    # read in the integers
-    num1 = list1[0] * list2[0]
-    num2 = list1[1] * list2[1]
-    num3 = list1[2] * list2[2]
+    if len(list1) == len(list2):
+        num1 = split1[0] * split2[0]
+        num2 = split1[1] * split2[1]
+        num3 = split1[2] * split2[2]
 
-sum = num1 + num2 + num3
+        sum = num1+num2+num3
+        return sum
+    
+    else:
+        print("error")
 
-# call sum of produccts (return a value)
-# print the result
-print(sum)
+if __name__ == '__main__':
+    a = input()
+    b = input()
+
+    total = sum_of_products(a,b)
+    #test
+    print(total)
